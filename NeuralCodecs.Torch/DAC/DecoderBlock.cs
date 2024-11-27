11d670cb1ceb0e234affdffe59864566b3605034
DillionLowry <dillionleel@gmail.com>
2024-11-26 18:41:34 -0600

    DAC encoder and decoder blocks

---
+++ NeuralCodecs.Torch/DAC/DecoderBlock.cs
@@ -0,0 +1,33 @@
+using NeuralCodecs.Torch.Modules;
+using TorchSharp.Modules;
+using static NeuralCodecs.Torch.SNAC;
+using static TorchSharp.torch;
+using static TorchSharp.torch.nn;
+
+namespace NeuralCodecs.Torch;
+
+public class DecoderBlock : Module<Tensor, Tensor>
+{
+    private readonly Sequential block;
+
+    public DecoderBlock(int inputDim = 16, int outputDim = 8, int stride = 1)
+        : base($"DecoderBlock_{inputDim}_{outputDim}")
+    {
+        block = Sequential(
+            new Snake1d(inputDim),
+            new WNConvTranspose1d(
+                inputDim,
+                outputDim,
+                kernelSize: 2 * stride,
+                stride: stride,
+                padding: (int)Math.Ceiling(stride / 2.0)
+            ),
+            new ResidualUnit(outputDim, dilation: 1),
+            new ResidualUnit(outputDim, dilation: 3),
+            new ResidualUnit(outputDim, dilation: 9)
+        );
+        RegisterComponents();
+    }
+
+    public override Tensor forward(Tensor x) => block.forward(x);
+}
---
+++ NeuralCodecs.Torch/DAC/EncoderBlock.cs
@@ -0,0 +1,32 @@
+using NeuralCodecs.Torch.Modules;
+using TorchSharp.Modules;
+using static NeuralCodecs.Torch.SNAC;
+using static TorchSharp.torch;
+using static TorchSharp.torch.nn;
+
+namespace NeuralCodecs.Torch;
+
+public class EncoderBlock : Module<Tensor, Tensor>
+{
+    private readonly Sequential block;
+
+    public EncoderBlock(int dim = 16, int stride = 1) : base($"EncoderBlock_{dim}")
+    {
+        block = Sequential(
+            new ResidualUnit(dim / 2, dilation: 1),
+            new ResidualUnit(dim / 2, dilation: 3),
+            new ResidualUnit(dim / 2, dilation: 9),
+            new Snake1d(dim / 2),
+            new WNConv1d(
+                dim / 2,
+                dim,
+                kernelSize: 2 * stride,
+                stride: stride,
+                padding: (int)Math.Ceiling(stride / 2.0)
+            )
+        );
+        RegisterComponents();
+    }
+
+    public override Tensor forward(Tensor x) => block.forward(x);
+}
