1dcfdf0161394646e6dcca8f0265de00283af1e7
DillionLowry <dillionleel@gmail.com>
2024-12-17 19:16:47 -0600

    DAC Unpickler and weights

---
+++ NeuralCodecs.Torch/Config/DAC/DACWeights.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TorchSharp.torch;
+
+namespace NeuralCodecs.Torch.Config.DAC
+{
+    public class DACWeights
+    {
+        public Dictionary<string, Tensor> StateDict { get; set; }
+        public Dictionary<string, object> Metadata { get; set; }
+    }
+}
