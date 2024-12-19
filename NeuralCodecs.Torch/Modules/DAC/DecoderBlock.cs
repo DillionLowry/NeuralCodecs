3a73e6b8f5c609938593717361e1dc41eb918c33
DillionLowry <dillionleel@gmail.com>
2024-12-19 10:17:21 -0600

    add disposal pattern, comments

--- NeuralCodecs.Torch/Modules/DAC/DecoderBlock.cs
+++ NeuralCodecs.Torch/Modules/DAC/DecoderBlock.cs
@@ -5,10 +5,20 @@ using static TorchSharp.torch.nn;
 
 namespace NeuralCodecs.Torch.Modules.DAC;
 
-public class DecoderBlock : Module<Tensor, Tensor>
+/// <summary>
+/// Represents a decoder block in the DAC (Dilated Audio Codec) architecture.
+/// </summary>
+public class DecoderBlock : Module<Tensor, Tensor>, IDisposable
 {
     private readonly Sequential block;
+    private bool disposedValue;
 
+    /// <summary>
+    /// Initializes a new instance of the DecoderBlock class.
+    /// </summary>
+    /// <param name="inputDim">The input dimension size. Default is 16.</param>
+    /// <param name="outputDim">The output dimension size. Default is 8.</param>
+    /// <param name="stride">The stride value for transposed convolution. Default is 1.</param>
     public DecoderBlock(int inputDim = 16, int outputDim = 8, int stride = 1)
         : base($"DecoderBlock_{inputDim}_{outputDim}")
     {
@@ -28,5 +38,35 @@ public class DecoderBlock : Module<Tensor, Tensor>
         RegisterComponents();
     }
 
+    /// <summary>
+    /// Performs the forward pass of the decoder block.
+    /// </summary>
+    /// <param name="x">The input tensor.</param>
+    /// <returns>The processed output tensor.</returns>
     public override Tensor forward(Tensor x) => block.forward(x);
+
+    /// <summary>
+    /// Disposes the resources used by this decoder block.
+    /// </summary>
+    /// <param name="disposing">True to dispose managed resources.</param>
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                block.Dispose();
+            }
+            disposedValue = true;
+        }
+    }
+
+    /// <summary>
+    /// Disposes the resources used by this decoder block.
+    /// </summary>
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
 }
